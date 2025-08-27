7d592493e74027b058aa2ff512d1dd112076f8ae
santoshtmp7 <santoshtmp7@gmail.com>
2025-08-27 15:01:39 +0545

    update

--- classes/local/helper.php
+++ classes/local/helper.php
@@ -75,6 +75,7 @@ class helper {
             $ch = curl_init();
             curl_setopt($ch, CURLOPT_URL, $CFG->wwwroot . '/customcleanurl/routetest');
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_TIMEOUT, 10);
             $response = curl_exec($ch);
             $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
             curl_close($ch);
